ec0ee4b045bb9554ab62a729dcfa3d03cffc724b
Alex Kemper <Kemper@Alexanders-MacBook-Air.local>
2020-06-11 18:57:53 -0400

    added change date button functionality

--- src/App.js
+++ src/App.js
@@ -8,11 +8,26 @@ import {API_KEY} from "./Constants/index"
 
 function App() {
 
+  const dateArray = [
+    '2020-06-11',
+    '2020-06-10',
+    '2020-06-09',
+    '2020-06-08',
+  ]
+
   const [nasaData, setNasaData] = useState('')
+  const [date, setDate] = useState(dateArray[0])
+  const [dateIndex, setDateIndex] = useState(0)
+
+  const dateButtonHandler = event => {
+    setDateIndex(dateIndex + 1)
+    setDate(dateArray[dateIndex])
+  }
+ 
 
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
       .then(res => {
         console.log(res.data)
         setNasaData(res.data)
@@ -23,17 +38,21 @@ function App() {
       .finally(() => {
         console.log("done")
       })
-  },[])
+  },[date])
 
 
   return (
     <div className="App">
       <Header />
       <Card cardData={nasaData}/>
+      <button onClick={dateButtonHandler}> 
+        Click here for {date}'s picture
+      </button>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+
     </div>
   );
 }
